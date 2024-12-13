b662c5f80561a353b4067603376b866ce0828aa4
ouchinao <ouchi.nao.jg@tut.jp>
2024-12-13 15:09:36 +0900

    :tada: first commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+//
+//  device_typeを指定
+//
+//  Created by 大内直 on 2024/08/24.
+//
+
+import 'package:flutter/material.dart';
+import 'device_type.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+
+void main() {
+  runApp(PortfolioApp(),
+  );
+}
+
+class PortfolioApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ouchiのポートフォリオ',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 64,
+              ),
+              Text(
+                'Ouchi Nao',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                'I\'m interested in robotics development or web application development.',
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Text(
+                'Hobby', 
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                'Travel, Harry Potter, Sauna, etc...',
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Text(
+                'Using',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                'python, C++, Dart, flutter, ROS2, Docker, etc...',
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Text(
+                'Study',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                'communication robotics',
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Text(
+                'Contact',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                'ouchi.nao.jg@tut.jp',
+              ),
+              SizedBox(
+                height: 64,
+              ),
+              GestureDetector(
+                onTap: () {
+                  launch(
+                    'https://github.com/ouchinao',
+                  );
+                },
+                child: Image.asset(
+                  'assets/GitHub-Logo.png',
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
